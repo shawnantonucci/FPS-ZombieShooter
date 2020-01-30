f620219ef352e8a307636de89a8afd100c065024
Shawn Antonucci <shawnantonucci2018@gmail.com>
2020-01-29 21:50:17 -0500

    setup repo

---
+++ Assets/GameDevHQ/FileBase/3D/Characters/Humans/Zombie_Sample/Scripts/Spawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject zombie;
+    public float delayTime = 4f;
+
+    IEnumerator Start()
+    {
+        var obj = Instantiate(zombie, transform.position, transform.rotation) as GameObject;
+        yield return new WaitForSeconds(delayTime);
+        StartCoroutine(Start());
+    }
+}
